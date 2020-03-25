e30a86b9308b5b63de6507ca77808278f9d44972
Ivan Martinez <ivanmxdev@gmail.com>
2020-03-24 22:57:36 -0700

    Bot topics response

---
+++ TODO.md
@@ -0,0 +1,20 @@
+# TODO
+
+- Los usuarios podran enviar sugerencias sobre temas.
+  - Listar todos los temas
+  - Agregar tema
+  - Votar tema
+
+[√] Agregar modelo para tema
+[√] Conectar a base de datos
+[] Metodos de listado
+
+- Formatear response
+  [] Metodo para agregar sugerencia
+  [] Metodo para votar sugerencia
+
+{
+id: int,
+title: string
+votes: int,
+}
--- server.js
+++ server.js
@@ -1,14 +1,58 @@
-const Discord  = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const { prefix, token } = require('./config.json');
+const { prefix, token } = require("./config.json");
+// const request = require('request');
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/local", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
-client.on('ready', () => {
+// define schema
+let topicSchema = new mongoose.Schema({
+  title: String,
+  votes: Number
+});
+
+let db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.on("open", function() {
+  console.log("Connected");
+});
+
+client.on("ready", () => {
   console.log(`Logged ${client.user.tag}`);
 });
 
-client.on('message', msg => {
+client.on("message", msg => {
   if (msg.content === `${prefix}ping`) {
-    msg.reply('Pong!');
+    msg.reply("Pong!");
+  } else if (msg.content === `${prefix}server`) {
+    msg.channel.send(`Info del server: ${msg.guild.name}`);
+  } else if (msg.content === `${prefix}led`) {
+    msg.reply("Esto se prendera!");
+  } else if (msg.content === `${prefix}topics`) {
+    let Topic = mongoose.model("Topic", topicSchema);
+
+    Topic.find(function(error, topics) {
+      if (error) return console.error(error);
+      msg.reply(topics.join());
+      return;
+    });
+  } else if (msg.content.startsWith(`${prefix}add`)) {
+    let title = msg.content.slice(5);
+
+    let Topic = mongoose.model("Topic", topicSchema);
+
+    let topic = new Topic({
+      title: title,
+      votes: 0
+    });
+
+    topic.save(function(error, item) {
+      if (error) return console.error(error);
+      msg.reply(item);
+    });
   }
 });
 
